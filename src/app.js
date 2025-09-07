002e6a289330f532990ad717c096ff754a6451a6
Aayush Mathur <mathuraayush15@gmail.com>
2025-09-07 18:28:21 +0530

    All setup Configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,9 @@ app.use(cookieParser())
 
 
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,7 @@
-const asyncHandler =(fn)=> async (req,res,next)=>{
-    try{
-        await fn(req,res,next)
-    } catch (error) {
-        res.status(err.code || 500).json({
-            success: false,
-            message: err.message
-        })
-    }
-}
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch(next);
+    };
+};
+
+export default asyncHandler;
