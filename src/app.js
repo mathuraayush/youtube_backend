08ead1ddd0bf745f3416a399701bb8761d9a7976
Aayush Mathur <mathuraayush15@gmail.com>
2026-01-15 17:52:27 +0530

    Update CORS origin to specific frontend URL

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 const app =express()
 app.use(cors({
-  origin: process.env.CORS_ORIGIN.split(","),
+  origin: "https://vidora-frontend-pi.vercel.app",
   credentials: true
 }));
 
