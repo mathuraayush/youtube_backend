1002c7fc47cab20c965009ece9e4070c8e8c0866
Aayush Mathur <mathuraayush15@gmail.com>
2026-01-17 04:37:49 +0000

    Refactor import statements in video.controller.js for consistency

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,8 +1,8 @@
-
+import  asyncHandler  from "../utils/asyncHandler.js";
 import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+
 import {uploadOnCloudinary} from "../utils/fileUpload.js"
 
 // Uploading Video with Thubnail,title,description, tags.
