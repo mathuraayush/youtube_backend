a4f89b9b4e28074d613c142ba66f32709d5cb14c
Aayush Mathur <mathuraayush15@gmail.com>
2026-02-20 11:11:28 +0000

    Refactor code structure for improved readability and maintainability

--- src/app.js
+++ src/app.js
@@ -5,10 +5,7 @@ import cookieParser from "cookie-parser"
 
 const app =express()
 app.set("trust proxy",1);
-app.use(cors({
-  origin: process.env.CORS_ORIGIN.split(","),
-  credentials: true
-}));
+app.use(cors());
 
 
 app.use(express.json({limit: "16kb"}))
